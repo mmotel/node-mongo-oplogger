0eb3213fed641db466318c47dd78935022b8dae8
mmotel <mateuszmotel@gmail.com>
2014-06-27 13:25:00 +0200

    added 1st Oplogger.tail impl

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ var Oplogger = require('./lib/oplogger/main.js');
 
 var MONGO_URL = 'mongodb://localhost:27017/testdb';
 var MONGO_OPLOG_URL = 'mongodb://oplogger:superhaslo123@localhost:27017/local?authSource=admin';
+
+var Oplog = Oplogger.tail( MONGO_URL, MONGO_OPLOG_URL,
+              { 'db': 'testdb', 'colls': [ 'category' ] } );
