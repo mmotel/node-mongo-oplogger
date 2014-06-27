423ec23526c1e352d5aaf2b7907507cb381a6a7b
mmotel <mateuszmotel@gmail.com>
2014-06-27 14:26:24 +0200

    added 1st operations handlers impl

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ var MONGO_OPLOG_URL = 'mongodb://oplogger:superhaslo123@localhost:27017/local?au
 
 var Oplog = Oplogger.tail( MONGO_URL, MONGO_OPLOG_URL,
               { 'db': 'testdb', 'colls': [ 'category' ] } );
+
+Oplog.onInsert( function ( item ) {
+  console.log( item );
+});
+
+Oplog.onUpdate( function ( item ) {
+  console.log( item );
+});
+
+Oplog.onRemove( function ( item ) {
+  console.log( item );
+});
