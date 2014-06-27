fd448bc2016e15a11b8895f80bea7e5f404a8ddb
mmotel <mateuszmotel@gmail.com>
2014-06-27 12:50:32 +0200

    basic structure

---
+++ app.js
@@ -0,0 +1,5 @@
+
+var Oplogger = require('./lib/oplogger/main.js');
+
+var MONGO_URL = 'mongodb://localhost:27017/testdb';
+var MONGO_OPLOG_URL = 'mongodb://oplogger:superhaslo123@localhost:27017/local?authSource=admin';
